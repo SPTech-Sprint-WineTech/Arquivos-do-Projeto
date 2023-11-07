bb1740f55f17791f17e90ef7091594046a41217c
guilhermecruci <guilherme.cruci@sptech.school>
2023-11-07 15:46:42 -0300

    Inclusão de um arquivo JS para transferir o script atualmente no arquivo index.html do diretório de login e fromtação de código no projeto todo!

---
+++ Entregáveis/Algoritmos/Site Institucional/Login/script.js
@@ -0,0 +1,16 @@
+function logar() {
+    var username_saved = "winetech";
+    var password_saved = "saoroque";
+
+    var usuario = document.getElementById("username");
+    var senha = document.getElementById("password");
+
+
+    if (usuario.value == username_saved && senha.value == password_saved) {
+        window.location.href = "../Dashboard/index.html"
+    }
+    else {
+        alert(`Senha e/ou Usuário inválido!`)
+    }
+
+}
