3ad671bdf11fc44e51af1b4b93fbaf8e49001d12
Henrique-Crispino <henrique.crispino@sptech.school>
2023-11-20 16:49:29 -0300

    Mudanças no css da dashboarding.

--- Entregáveis/Algoritmos/Site Institucional/dashboarding/script.js
+++ Entregáveis/Algoritmos/Site Institucional/dashboarding/script.js
@@ -2,13 +2,11 @@ var container_menu = document.getElementsByClassName('container_menu_bar')[0];
 var icone_menu = document.getElementsByClassName('ph-list')[0];
 
 function ver_menu() {
-  if (container_menu.style.display === 'flex') {
-    container_menu.style.display = 'none';
-    icone_menu.style.transform = 'rotate(0deg)';
-  } else {
-    container_menu.style.display = 'flex';
-    icone_menu.style.transform = 'rotate(90deg)';
-  }
+  container_menu.style.display = 'flex';
+}
+
+function fechar_menu() {
+  container_menu.style.display = 'none';
 }
 
 
